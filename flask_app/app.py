62466a01634d58fe3676e1f859a04079d4ffb12a
rxshmi-p <rxshmi-p@github.com>
2024-03-29 19:45:32 -0700

    add css for app

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Voice Cloning App!'
+def main():
+    return render_template('temp1.html')
 
 if __name__ == '__main__':
     app.run()
