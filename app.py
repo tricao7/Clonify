cc1d64ba321b297332cb00d74fcd2495bc209f62
Tri Cao <tcao5555@gmail.com>
2024-04-21 20:15:03 -0700

    update app.py

--- app.py
+++ app.py
@@ -65,7 +65,21 @@ def main():
     # Display image and center
     st.image("images/Clonify.png", use_column_width=True)
     # Display title
-    st.title("Voice Cloning App")
+    st.title("Welcome to Clonify!")
+    # Display description
+    st.write(
+        "Clonify is a text-to-speech (TTS) application that allows you to generate voice from text. \
+        You can upload a .wav file and enter text to be spoken. \
+        The generated audio will be a voice clone of the uploaded file."
+    )
+    st.write("Instructions:")
+    st.write(
+        "1. Upload a .wav file that you want to clone the voice of with the prompt: 'The North Wind and the Sun were disputing which was the stronger, when a traveler came along wrapped in a warm cloak.\
+        They agreed that the one who first succeeded in making the traveler take his cloak off should be considered stronger than the other.\
+        Then the North Wind blew as hard as he could, but the more he blew the more closely did the traveler fold his cloak around him;\
+        and at last the North Wind gave up the attempt. Then the Sun shined out warmly, and immediately the traveler took off his cloak.\
+        And so the North Wind was obliged to confess that the Sun was the stronger of the two.'."
+    )
     # Get uploaded file from user
     uploaded_file = st.file_uploader("Upload a .wav file", type=".wav")
 
