bab967d411bb0949de3ddb5fa6471198d2433d2c
rxshmi-p <rpanse@dons.usfca.edu>
2024-04-30 20:36:51 -0700

    setting up user_auth

--- pages/user_auth.py
+++ pages/user_auth.py
@@ -1,1 +1,20 @@
 import streamlit as st
+import pymongo
+from pymongo.server_api import ServerApi
+
+# Connect to the DB.
+@st.cache_data
+def connect_db():
+    client = pymongo.MongoClient(
+      st.secrets["mongo"]["connection_url"], 
+      server_api=ServerApi('1'))
+    db = client.get_database('main')
+    return db.users
+
+user_db = connect_db()
+
+# Initialize Session States.
+if 'username' not in st.session_state:
+       st.session_state.username = ''
+if 'form' not in st.session_state:
+       st.session_state.form = ''
