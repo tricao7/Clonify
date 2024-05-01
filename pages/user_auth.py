98722ff59d496c945b53fc8b6595c97ca2a49596
rxshmi-p <rpanse@dons.usfca.edu>
2024-04-30 21:28:38 -0700

    sign up form added and connected to mongo

--- pages/user_auth.py
+++ pages/user_auth.py
@@ -19,20 +19,67 @@ if 'username' not in st.session_state:
 if 'form' not in st.session_state:
        st.session_state.form = ''
 
-# Sign up form.
-def signup():
-    st.title('Sign up')
-    username = st.text_input('Username')
-    password = st.text_input('Password', type='password')
-    if st.button('Sign up'):
-        if user_db.find_one({'username': username}):
-            st.error('User already exists')
-        else:
-            user_db.insert_one({'username': username, 'password': password})
-            st.success('User created')
-            st.session_state.username = username
-            st.session_state.form = 'app'
+def select_signup():
+    st.session_state.form = 'signup_form'
 
+def user_update(name):
+    st.session_state.username = name
+
+if st.session_state.username != '':
+    st.sidebar.write(f"You are logged in as {st.session_state.username.upper()}")
+
+# Initialize Sing In or Sign Up forms
+if st.session_state.form == 'signup_form' and st.session_state.username == '':
+  
+    signup_form = st.sidebar.form(key='signup_form', clear_on_submit=True)
+    new_username = signup_form.text_input(label='Enter Username*')
+    new_user_email = signup_form.text_input(label='Enter Email Address*')
+    new_user_pas = signup_form.text_input(label='Enter Password*', type='password')
+    user_pas_conf = signup_form.text_input(label='Confirm Password*', type='password')
+    note = signup_form.markdown('**required fields*')
+    signup = signup_form.form_submit_button(label='Sign Up')
+    
+    if signup:
+        if '' in [new_username, new_user_email, new_user_pas]:
+            st.sidebar.error('Some fields are missing')
+        else:
+            if user_db.find_one({'log' : new_username}):
+                st.sidebar.error('Username already exists')
+            if user_db.find_one({'email' : new_user_email}):
+                st.sidebar.error('Email is already registered')
+            else:
+                if new_user_pas != user_pas_conf:
+                    st.sidebar.error('Passwords do not match')
+                else:
+                    user_update(new_username)
+                    user_db.insert_one({'log' : new_username, 'email' : new_user_email, 'pass' : new_user_pas})
+                    st.sidebar.success('You have successfully registered!')
+                    st.sidebar.success(f"You are logged in as {new_username.upper()}")
+                    del new_user_pas, user_pas_conf
+                    
+elif st.session_state.username == '':
+    login_form = st.sidebar.form(key='signin_form', clear_on_submit=True)
+    username = login_form.text_input(label='Enter Username')
+    user_pas = login_form.text_input(label='Enter Password', type='password')
+    
+    if user_db.find_one({'log' : username, 'pass' : user_pas}):
+        login = login_form.form_submit_button(label='Sign In', on_click=user_update(username))
+        if login:
+            st.sidebar.success(f"You are logged in as {username.upper()}")
+            del user_pas
+    else:
+        login = login_form.form_submit_button(label='Sign In')
+        if login:
+            st.sidebar.error("Username or Password is incorrect. Please try again or create an account.")
+else:
+    logout = st.sidebar.button(label='Log Out')
+    if logout:
+        user_update('')
+        st.session_state.form = ''
+
+# 'Create Account' button
+if st.session_state.username == "" and st.session_state.form != 'signup_form':
+    signup_request = st.sidebar.button('Create Account', on_click=select_signup)
 
 if __name__ == "__main__":
-    signup()
+    select_signup()
