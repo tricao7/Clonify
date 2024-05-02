9e928cb7b5f1b43bc715025963002c3a1cd5ad75
cho-amy <amycho99@gmail.com>
2024-05-01 17:50:48 -0700

    front end

--- Hello.py
+++ Hello.py
@@ -4,51 +4,100 @@ import streamlit as st
 
 
 def main():
-    # Display image and center
-    st.set_page_config(initial_sidebar_state="collapsed")
+    st.set_page_config(page_title="Landing Page",
+                       initial_sidebar_state="collapsed")
 
-    st.markdown(
-        """
+    st.markdown("""
         <style>
+        @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Sen:wght@400..800&display=swap');
+        
+        .stButton>button {
+            border: 2px solid #8A2BE2;
+            border-radius: 5px;
+            background-color: #951CCE;
+            color: white;
+            font-size: 16px;
+            padding: 10px 24px;
+            transition: background-color 0.3s, color 0.3s;
+        }
+        .stButton>button:hover {
+            background-color: white;
+            color: #951CCE;
+        }
+        
         [data-testid="collapsedControl"] {
-        display: none
+            display: none;
+        }
+        .title {
+            text-align: center;
+        }
+        
+        .body {
+            font-size: 16px;
+            font-family: 'Montserrat', sans-serif;
+            font-optical-sizing: auto;
+            font-weight: 100;
+            font-style: thin;
+        }
+        
+        .header {
+           font-family: 'Sen', sans-serif;
+           font-size: 35px;
         }
         </style>
-        """,
-        unsafe_allow_html=True,
-    )
+        """, unsafe_allow_html=True)
 
-    st.image("images/Clonify.png", use_column_width=True)
+    st.image("images/clonify.png", use_column_width=True)
+    st.markdown('<h1 class="title">Elevate Your Audio Experience with AI-Generated Voices</h1>',
+                unsafe_allow_html=True)
+    col1, col2 = st.columns(2)
 
-    # Display title
-    st.title("Welcome to Clonify!")
+    with col1:
+        st.markdown('<div class="body">Let your content go beyond text with our realistic TTS generation models. Generate your own custom high-quality audio.</div>',
+                    unsafe_allow_html=True)
+        st.markdown("""<br>""", unsafe_allow_html=True)
+        if st.button("Try It Now"):
+            st.switch_page("pages/app.py")
+
+    with col2:
+        st.markdown('<div class="body">Join us to personalize your experience and access premium features like access to past voices and audio outputs!</div>',
+                    unsafe_allow_html=True)
+        st.markdown("""<br>""", unsafe_allow_html=True)
+
+        if st.button("Sign Up / Log In"):
+            st.switch_page("pages/user_auth.py")
+
     st.markdown("---")
-
-    # Display description
-    st.write(
-        "Clonify is a text-to-speech (TTS) application that allows you to generate voice from text. \
-        You can upload a .wav file and enter text to be spoken. \
-        The generated audio will be a voice clone of the uploaded file."
-    )
-    st.write("Instructions:")
-    st.write("1. Upload a 30 second .wav file that you want to clone the voice of.")
+    st.markdown('<div class="header">How It Works</div>',
+                unsafe_allow_html=True)
+    st.markdown('<div class="body">Clonify is a text-to-speech (TTS) application that allows you to generate voice from text. You can upload a .wav file and enter text to be spoken. The generated audio will be a voice clone of the uploaded file.</div>', unsafe_allow_html=True)
+    st.markdown("""<br>""", unsafe_allow_html=True)
+    st.markdown('<div class="body">Follow these simple steps to try it yourself:</h1>',
+                unsafe_allow_html=True)
+    st.markdown("""<br>""", unsafe_allow_html=True)
+    st.markdown('<div class="body">1. Record a 30 second .wav file using the voice you want to clone.</h1>',
+                unsafe_allow_html=True)
     with st.expander("Click here for a sample prompt (for best results): "):
-        st.write(
-            "   'The North Wind and the Sun were disputing which was the stronger, when a traveler came along wrapped in a warm cloak.\
+        st.markdown('<div class="body">The North Wind and the Sun were disputing which was the stronger, when a traveler came along wrapped in a warm cloak.\
             They agreed that the one who first succeeded in making the traveler take his cloak off should be considered stronger than the other.\
             Then the North Wind blew as hard as he could, but the more he blew the more closely did the traveler fold his cloak around him;\
             and at last the North Wind gave up the attempt. Then the Sun shined out warmly, and immediately the traveler took off his cloak.\
-            And so the North Wind was obliged to confess that the Sun was the stronger of the two.'"
-        )
-    st.write("2. Enter the text you want to be spoken.")
-    st.write("3. Adjust the guidance, top P, and top K parameters.")
-    st.write("4. Click the 'Generate Voice' button to generate the audio.")
-    if st.button("Demo"):
-        st.switch_page("pages/app.py")
-    if st.button("Sign up/Log in"):
-        st.switch_page("pages/user_auth.py")
-    # Display horizontal line
+            And so the North Wind was obliged to confess that the Sun was the stronger of the two.</h1>', unsafe_allow_html=True)
+    st.markdown('<div class="body">2. Click the Try it Now button above to redirect to our free demo.</h1>',
+                unsafe_allow_html=True)
+    st.markdown('<div class="body">3. Enter the custom text you want to be spoken.</h1>',
+                unsafe_allow_html=True)
+    st.markdown('<div class="body">4. Adjust the guidance, top P, and top K parameters.</h1>',
+                unsafe_allow_html=True)
+    st.markdown('<div class ="body">5. Click the "Generate Voice" button to generate your new customized audio.</h1>', unsafe_allow_html=True)
     st.markdown("---")
+
+    # # Footer Section
+    # st.markdown('<h1 class="header">Get Started with Clonify AI Voice Generator</h1>',
+    #             unsafe_allow_html=True)
+
+    # st.write(
+    #     "Ready to bring your content to life? [Sign up today](/user_auth) or contact us for more information.")
 
 
 if __name__ == "__main__":
--- pages/user_auth.py
+++ pages/user_auth.py
@@ -2,75 +2,108 @@ import streamlit as st
 import pymongo
 from pymongo.server_api import ServerApi
 
+st.set_page_config(page_title="User Sign Up/Log In")
+st.markdown("""
+        <style>
+        .stButton>button {
+            border: 2px solid #8A2BE2;
+            border-radius: 5px;
+            background-color: #951CCE;
+            color: white;
+            font-size: 16px;
+            padding: 10px 24px;
+            transition: background-color 0.3s, color 0.3s;
+        }
+
+        .stButton>button:hover {
+            background-color: white; 
+            color: #951CCE;
+        }
+        </style>
+        """, unsafe_allow_html=True)
+
 # Connect to the DB.
+
+
 @st.cache_resource
 def connect_db():
     client = pymongo.MongoClient(
-      st.secrets["mongo"]["connection_url"], 
-      server_api=ServerApi('1'))
+        st.secrets["mongo"]["connection_url"],
+        server_api=ServerApi('1'))
     db = client.get_database('main')
     return db.users
+
 
 user_db = connect_db()
 
 # Initialize Session States.
 if 'username' not in st.session_state:
-       st.session_state.username = ''
+    st.session_state.username = ''
 if 'form' not in st.session_state:
-       st.session_state.form = ''
+    st.session_state.form = ''
+
 
 def select_signup():
     st.session_state.form = 'signup_form'
 
+
 def user_update(name):
     st.session_state.username = name
 
+
 if st.session_state.username != '':
-    st.sidebar.write(f"You are logged in as {st.session_state.username.upper()}")
+    st.sidebar.write(
+        f"You are logged in as {st.session_state.username.upper()}")
 
 # Initialize Sing In or Sign Up forms
 if st.session_state.form == 'signup_form' and st.session_state.username == '':
-  
+
     signup_form = st.sidebar.form(key='signup_form', clear_on_submit=True)
     new_username = signup_form.text_input(label='Enter Username*')
     new_user_email = signup_form.text_input(label='Enter Email Address*')
-    new_user_pas = signup_form.text_input(label='Enter Password*', type='password')
-    user_pas_conf = signup_form.text_input(label='Confirm Password*', type='password')
+    new_user_pas = signup_form.text_input(
+        label='Enter Password*', type='password')
+    user_pas_conf = signup_form.text_input(
+        label='Confirm Password*', type='password')
     note = signup_form.markdown('**required fields*')
     signup = signup_form.form_submit_button(label='Sign Up')
-    
+
     if signup:
         if '' in [new_username, new_user_email, new_user_pas]:
             st.sidebar.error('Some fields are missing')
         else:
-            if user_db.find_one({'log' : new_username}):
+            if user_db.find_one({'log': new_username}):
                 st.sidebar.error('Username already exists')
-            if user_db.find_one({'email' : new_user_email}):
+            if user_db.find_one({'email': new_user_email}):
                 st.sidebar.error('Email is already registered')
             else:
                 if new_user_pas != user_pas_conf:
                     st.sidebar.error('Passwords do not match')
                 else:
                     user_update(new_username)
-                    user_db.insert_one({'log' : new_username, 'email' : new_user_email, 'pass' : new_user_pas})
+                    user_db.insert_one(
+                        {'log': new_username, 'email': new_user_email, 'pass': new_user_pas})
                     st.sidebar.success('You have successfully registered!')
-                    st.sidebar.success(f"You are logged in as {new_username.upper()}")
+                    st.sidebar.success(
+                        f"You are logged in as {new_username.upper()}")
                     del new_user_pas, user_pas_conf
-                    
+
 elif st.session_state.username == '':
     login_form = st.sidebar.form(key='signin_form', clear_on_submit=True)
     username = login_form.text_input(label='Enter Username')
     user_pas = login_form.text_input(label='Enter Password', type='password')
-    
-    if user_db.find_one({'log' : username, 'pass' : user_pas}):
-        login = login_form.form_submit_button(label='Sign In', on_click=user_update(username))
+
+    if user_db.find_one({'log': username, 'pass': user_pas}):
+        login = login_form.form_submit_button(
+            label='Sign In', on_click=user_update(username))
         if login:
             st.sidebar.success(f"You are logged in as {username.upper()}")
             del user_pas
     else:
         login = login_form.form_submit_button(label='Sign In')
         if login:
-            st.sidebar.error("Username or Password is incorrect. Please try again or create an account.")
+            st.sidebar.error(
+                "Username or Password is incorrect. Please try again or create an account.")
 else:
     logout = st.sidebar.button(label='Log Out')
     if logout:
@@ -79,7 +112,8 @@ else:
 
 # 'Create Account' button
 if st.session_state.username == "" and st.session_state.form != 'signup_form':
-    signup_request = st.sidebar.button('Create Account', on_click=select_signup)
+    signup_request = st.sidebar.button(
+        'Create Account', on_click=select_signup)
 
 if __name__ == "__main__":
     select_signup()
